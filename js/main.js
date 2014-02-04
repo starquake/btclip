fdd997f2c473c8abd5f6f284e4bc07ff28b41a9a
Jan Visser <starquake@linuxeverywhere.org>
2014-02-04 15:42:31 +0100

    Added local jquery files Added settings screen to navigation button Added support for multiple files Added some checks to the path handling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Load native UI library
 var gui = require('nw.gui');
 var os = require("os");
+var path = require("path");
 var clipboard = gui.Clipboard.get();
 
 var holderEl = document.getElementById('holder');
 
-var pathDelimiter = '/';
 var basePathEl = document.getElementById("basePath");
 var baseUrlEl = document.getElementById("baseUrl");
 var basePathExampleEl = document.getElementById("basePathExample");
@@ -15,13 +15,13 @@ var openBasePathEl = document.getElementById("openBasePath");
 switch (os.platform()) {
     case 'win32':
         basePathExampleEl.innerHTML = 'C:\\Users\\John\\BTSync\\Public\\';
-        pathDelimiter = '\\';
         break;
     case 'darwin':
         basePathExampleEl.innerHTML = '/Users/John/BTSync/Public/';
         break;
     default: // linux
         basePathExampleEl.innerHTML = '/home/john/BTSync/Public/';
+        break;
 }
 
 
@@ -34,20 +34,19 @@ function escapeRegExp(string) {
 }
 
 openBasePathEl.onclick = function (e) {
-    if (basePathEl.value.slice(-1) !== pathDelimiter) {
-        basePathEl.value = basePathEl.value + pathDelimiter;
-    }
-    gui.Shell.showItemInFolder(basePathEl.value);
-}
+    basePathEl.value = path.normalize(basePathEl.value);
+    basePathEl.value = path.dirname(basePathEl.value + path.sep + '.');
+    gui.Shell.openItem(basePathEl.value);
+};
 
 // prevent default behavior from changing page on dropped file
 window.ondragover = function (e) {
     e.preventDefault();
-    return false
+    return false;
 };
 window.ondrop = function (e) {
     e.preventDefault();
-    return false
+    return false;
 };
 
 
@@ -62,22 +61,38 @@ holderEl.ondragend = function () {
 holderEl.ondrop = function (e) {
     e.preventDefault();
     this.className = '';
-
+    var urls = [];
     for (var i = 0; i < e.dataTransfer.files.length; ++i) {
         var originalFullPath = e.dataTransfer.files[i].path;
-        var relativePath = originalFullPath.replace(new RegExp("^" + escapeRegExp(basePathEl.value), "g"), '');
-        if (os.platform() === 'win32') {
-            relativePath = relativePath.replace('\\', '/');
-        }
-        if (baseUrlEl.value.slice(-1) !== '/') {
-            baseUrlEl.value = baseUrlEl.value + '/';
+
+        basePathEl.value = path.normalize(basePathEl.value);
+        basePathEl.value = path.dirname(basePathEl.value + path.sep + '.');
+
+        if (originalFullPath.indexOf(basePathEl.value) === 0) {
+
+            var relativePath = originalFullPath.replace(new RegExp("^" + escapeRegExp(basePathEl.value), "g"), '');
+
+            if (os.platform() === 'win32') {
+                relativePath = relativePath.replace(/\\/g, '/');
+            }
+            if (baseUrlEl.value && baseUrlEl.value.slice(-1) !== '/') {
+                baseUrlEl.value = baseUrlEl.value + '/';
+            }
+            var newUrl = baseUrlEl.value + encodeURI(relativePath);
+            urls.push(newUrl);
+
+            console.log(originalFullPath + ' -> ' + newUrl);
+
+        } else {
+            console.log(originalFullPath + ' not in ' + basePathEl.value + ', skipping...');
         }
 
-        var newUrl = baseUrlEl.value + encodeURI(relativePath);
-
-        console.log(originalFullPath + ' -> ' + newUrl);
-        clipboard.set(newUrl, 'text');
     }
+    if (urls.length > 0) {
+        clipboard.clear();
+        clipboard.set(urls.join(os.EOL), 'text');
+    }
+
     return false;
 };
 
